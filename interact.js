d59f8c946a2ca14e5261b00fe732163fd22bf2b2
Hermes J. Pappas <hjp@hjp.gr>
2020-07-19 14:31:15 +0300

    Add functionality for keeping calculating even after pressing equals

--- interact.js
+++ interact.js
@@ -5,53 +5,58 @@ let solved = false;
 
 buttons.forEach( button => {
 
-		button.addEventListener('click', event => {
+	button.addEventListener('click', event => {
 
-				let text = button.textContent.replace(/\s/g, "");
+			let text = button.textContent.replace(/\s/g, "");
 
-				if(button.classList.contains("num"))
+			if(button.classList.contains("num"))
+			{
+				if(solved === true)
 				{
-					if(solved === true)
-					{
-						solved = false;
-						display.textContent = "";
-					}
+					solved = false;
+					display.textContent = "";
+				}
 
-					display.textContent += text;
-				}
-				
-				else if(button.classList.contains("oper"))
-				{
-					//evaluate if operation needs to first calculate
-					//if we already have an operator in the string
-					if(/[\+\-\*\/]/g.test(display.textContent))
-					{
-						display.textContent = operate(display.textContent);
-					}
-					display.textContent += " " + text + " ";
-				}
-				
-				else if(button.id === "calctitle")
+				display.textContent += text;
+			}
+			
+			else if(button.classList.contains("oper"))
+			{
+				if(display.textContent === "")
 					return;
 				
-				else if(button.id === "clear")
-					display.textContent = "";
-				
-				else if(button.id === "equals")
+				if(solved === true)
+					solved = false;
+				//evaluate if operation needs to first calculate
+				//if we already have an operator in the string
+				if(/[\+\-\*\/]/g.test(display.textContent))
 				{
-					if(/[0-9]/.test(display.textContent
-								.slice(display.textContent.length-1, display.textContent.length)))
-					{
-						let result = operate(display.textContent);
-						result = Number((result).toFixed(3));
-						display.textContent = result;
-						solved = true;
-
-					}
-					
-					else
-						return;
+					display.textContent = operate(display.textContent);
 				}
+				display.textContent += " " + text + " ";
+			}
+			
+			else if(button.id === "calctitle")
+				return;
+			
+			else if(button.id === "clear")
+				display.textContent = "";
+			
+			else if(button.id === "equals")
+			{
+				if(/[0-9]/.test(display.textContent
+							.slice(display.textContent.length-1, display.textContent.length)))
+				{
+					let result = operate(display.textContent);
+					result = Number((result).toFixed(3));
+					display.textContent = result;
+					solved = true;
 
-		});
+				}
+				
+				else
+					return;
+			}
+
+	});
 });
