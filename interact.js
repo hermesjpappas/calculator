c3b20e2708767032b221fb17774ed0bc3b99a08b
Hermes J. Pappas <hjp@hjp.gr>
2020-07-27 10:15:18 +0300

    Add decimal button to HTML/CSS

--- interact.js
+++ interact.js
@@ -8,126 +8,126 @@ buttons.forEach( button => {
 
 	button.addEventListener('click', event => {
 
-			//strip div/button content of whitespace
-			let text = button.textContent.replace(/\s/g, "");
+		//strip div/button content of whitespace
+		let text = button.textContent.replace(/\s/g, "");
 
-			
-			//if a number button is pressed
-			if(button.classList.contains("num"))
-			{
-				//if we have a solved state, reset the numbers by
-				//just writing a new number in, instead of having
-				//to clear first
-
-				if(solved === true)
-				{
-					solved = false;
-					display.textContent = "";
-				}
-
-				//add the number to the eval string if valid,
-				//i.e. ends in a number or space
-				//if(/[0-9\s]?$/.test(display.textContent))
-					display.textContent += text;
-			}//end if number
-			
-			//if an operator button is pressed
-			else if(button.classList.contains("oper"))
-			{
-				//allows for continuous evaluation
-				if(solved === true && /[0-9]$/.test(display.textContent))
-					solved = false;
-	
-				//special treatment for our minus
-				if(text === "-")
-				{
-					//if there is nothing or there is a previous number
-					//and an operator, means it can be a negative number
-					//therefore allow adding "-" on numbers with no space
-
-					if(display.textContent === "" || 
-					/[0-9]+\s[\+\-\*\/]\s$/.test(display.textContent))
-					{
-						display.textContent += text;
-						solved = false;
-						
-						//return is so we don't add more to 
-						//the string by mistake
-						return;
-					}
-				}//end if minus
-			
-	
-				//if there are no numbers or 
-				//the string doesn't end with a number, do nothing
-				if(/-?[0-9]/g.test(display.textContent) === false 
-				|| /[0-9]$/.test(display.textContent) === false)
-					return;
-
-				//evaluate if operation needs to first calculate
-				//if we already have two numbers and an operator in the string
-				if(/-?[0-9]+\s[\+\-\*\/]+\s-?[0-9]+/g.test(display.textContent))
-				{
-					let result = operate(display.textContent);
-					
-					if(result === "Don't do that.")
-					{
-						display.textContent = result;
-						solved = true;
-					}
-					
-					else 
-					{
-						result = Number((result).toFixed(3));
-						display.textContent = result;
-						solved = false;
-					}
-				}	
-
-				//if after all that we have a digit at the end of the string
-				//then add the operator to the overall evaluation string
-				if(/[0-9]$/.test(display.textContent))
-					display.textContent += " " + text + " ";
-				
-			}//end if operator
-			
-			
-			//do nothing if clicking on the title
-			else if(button.id === "calctitle")
-				return;
 		
-			//clear everything if the clear button is pressed
-			else if(button.id === "clear")
+		//if a number button is pressed
+		if(button.classList.contains("num"))
+		{
+			//if we have a solved state, reset the numbers by
+			//just writing a new number in, instead of having
+			//to clear first
+
+			if(solved === true)
+			{
+				solved = false;
 				display.textContent = "";
-		
+			}
 
-			//if equals is pressed
-			else if(button.id === "equals")
+			//add the number to the eval string if valid,
+			//i.e. ends in a number or space
+			//if(/[0-9\s]?$/.test(display.textContent))
+				display.textContent += text;
+		}//end if number
+		
+		//if an operator button is pressed
+		else if(button.classList.contains("oper"))
+		{
+			//allows for continuous evaluation
+			if(solved === true && /[0-9]$/.test(display.textContent))
+				solved = false;
+	
+			//special treatment for our minus
+			if(text === "-")
 			{
-				//if there is a number at the end, evaluate
-				//otherwise do nothing
-				
-				if(/[0-9]$/.test(display.textContent))
+				//if there is nothing or there is a previous number
+				//and an operator, means it can be a negative number
+				//therefore allow adding "-" on numbers with no space
+
+				if(display.textContent === "" || 
+				/[0-9]+\s[\+\-\*\/]\s$/.test(display.textContent))
 				{
-					let result = operate(display.textContent);
+					display.textContent += text;
+					solved = false;
 					
-					if(result === "Don't do that.")
-					{
-						display.textContent = result;
-						solved = true;
-					}
-					
-					else 
-					{
-						result = Number((result).toFixed(3));
-						display.textContent = result;
-						solved = true;
-					}			
+					//return is so we don't add more to 
+					//the string by mistake
+					return;
+				}
+			}//end if minus
+		
+	
+			//if there are no numbers or 
+			//the string doesn't end with a number, do nothing
+			if(/-?[0-9]/g.test(display.textContent) === false 
+			|| /[0-9]$/.test(display.textContent) === false)
+				return;
+
+			//evaluate if operation needs to first calculate
+			//if we already have two numbers and an operator in the string
+			if(/-?[0-9]+\s[\+\-\*\/]+\s-?[0-9]+/g.test(display.textContent))
+			{
+				let result = operate(display.textContent);
+				
+				if(result === "Don't do that.")
+				{
+					display.textContent = result;
+					solved = true;
 				}
 				
-				else
-					return;
+				else 
+				{
+					result = Number((result).toFixed(3));
+					display.textContent = result;
+					solved = false;
+				}
+			}	
+
+			//if after all that we have a digit at the end of the string
+			//then add the operator to the overall evaluation string
+			if(/[0-9]$/.test(display.textContent))
+				display.textContent += " " + text + " ";
+			
+		}//end if operator
+		
+		
+		//do nothing if clicking on the title
+		else if(button.id === "calctitle")
+			return;
+	
+		//clear everything if the clear button is pressed
+		else if(button.id === "clear")
+			display.textContent = "";
+	
+
+		//if equals is pressed
+		else if(button.id === "equals")
+		{
+			//if there is a number at the end, evaluate
+			//otherwise do nothing
+			
+			if(/[0-9]$/.test(display.textContent))
+			{
+				let result = operate(display.textContent);
+				
+				if(result === "Don't do that.")
+				{
+					display.textContent = result;
+					solved = true;
+				}
+				
+				else 
+				{
+					result = Number((result).toFixed(3));
+					display.textContent = result;
+					solved = true;
+				}			
 			}
+			
+			else
+				return;
+		}
 
 	});
 });
